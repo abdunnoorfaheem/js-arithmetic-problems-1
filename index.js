7bcf18ff113c52626261aefb95b27a4c938778f2
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-09-17 15:09:37 +0600

    js arthmetic basic problem solve

---
+++ index.js
@@ -0,0 +1,75 @@
+//problem-1
+
+let saraHave = 25;
+let saraBought = 15;
+let total = saraHave + saraBought;
+// console.log("Sara have ,"total);
+
+//problem-2
+
+let jonHave = 500;
+let jonBuy = 200;
+
+let result = (jonHave - jonBuy);
+// console.log("Jon Have ", result);
+
+//problem-3
+
+let everyHour = 120;
+let targetHour = 8;
+let totalMachine = (everyHour * targetHour);
+// console.log("Total Machine Made", totalMachine);
+
+//problem-4
+
+let tree = 12;
+let row = 8;
+let addNewPlaningTree = 20;
+let totalTree = (tree * row);
+let gardenTotalTree = (totalTree + addNewPlaningTree);
+// console.log("Total Tree in the Garden ", gardenTotalTree);
+
+//problem-5
+
+let rahimHave = 5;
+let insideBox = 10;
+let rahimGiveFriend = 12;
+let rahimHas = (rahimHave * insideBox) - rahimGiveFriend;
+// console.log("Finally Rahim Has ", rahimHas);
+
+//problem-6
+
+let trainKmh = 150;
+let time = 3;
+let oneHour = (trainKmh / time);
+// console.log("One Hour train go ", oneHour);
+
+
+//problem-7
+
+let dailyMachineUse = 6;
+let dailyProductMade = 200;
+let oneDayMadeProduct = (dailyProductMade / dailyMachineUse);
+// console.log("One day Product made", oneDayMadeProduct);
+
+let addNewMachine = 2;
+
+let totalMachineUse = (dailyMachineUse + addNewMachine);
+
+// console.log("Total Machine add ", totalMachineUse);
+
+let totalProductions = (oneDayMadeProduct * totalMachineUse);
+// console.log("New Machine add and All Productions ", totalProductions);
+
+let notSellProduct = 3;
+let restProduct = (totalProductions - notSellProduct);
+// console.log("Final Product", parseInt(restProduct.toFixed(0)));
+
+//problem-8
+
+let buyChocolate = 120;
+let onePacketStore = 9;
+
+let packetStore = (buyChocolate % onePacketStore);
+
+// console.log("Final Chocolate ", packetStore);
